3d7af664ad1b4fdd4292b37b35d133d6a2c8ce03
Chris Chapman <chapmanchri@gmail.com>
2015-12-29 23:01:28 -0500

    add email validation and password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
 
+
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: {case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6}
   validates_presence_of :password, :email, :acceptance
   #
   validates_uniqueness_of :email
   #
-  has_secure_password
+
 
   has_many :events
 
